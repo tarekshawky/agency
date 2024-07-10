cc7033bf46c57f0ada29c13138bccbc627e7653f
tarek <tarekshawky2016@gmail.com>
2024-07-10 19:05:54 +0300

    Add  Component Recent works

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import HeroSection from "./components/HeroSection"
 import About from "./components/About"
+import RecentWorks from "@/app/components/RecentWorks";
 export default function Home() {
   return (
     <div>
       <HeroSection/>
         <About/>
+        <RecentWorks/>
     </div>
   );
 }
