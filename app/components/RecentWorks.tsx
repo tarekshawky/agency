90de473c460e0d4b8818094214378b78429a6de0
tarek <tarekshawky2016@gmail.com>
2024-07-10 19:05:38 +0300

    Add New Component Recent works

---
+++ app/components/RecentWorks.tsx
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+
+export default function RecentWorks() {
+    return (
+        <div>
+            <div className='flex justify-center'>
+                <h2 className='font-bold text-3xl text-[#5f687b]'>Featured Projects</h2>
+            </div>
+        </div>
+    );
+}
