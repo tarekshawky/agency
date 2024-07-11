3c38d15604565572863afc6720a9a01ec8b54004
tarek <tarekshawky2016@gmail.com>
2024-07-11 19:45:12 +0300

    Add meta tag

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,15 @@ import HeroSection from "./components/HeroSection"
 import About from "./components/About"
 import RecentWorks from "@/app/components/RecentWorks";
 import Skills from "@/app/components/Skills";
+import Head from 'next/head';
+
 export default function Home() {
   return (
     <div>
+        <Head>
+            <meta name="google-site-verification" content="QB7w0bIGg_3VBosfpXwaiVc1c0hIxpQ0qRe6DC4Muj8" />
+            {/* Other meta tags */}
+        </Head>
       <HeroSection/>
       <About/>
       <RecentWorks/>
