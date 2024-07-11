e1daef046ba001a6bcec2eba440c46e9d0e68dc0
tarek <tarekshawky2016@gmail.com>
2024-07-11 20:07:30 +0300

    Add meta tag

---
+++ pages/_document.js
@@ -0,0 +1,20 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+    render() {
+        return (
+            <Html>
+                <Head>
+                    <meta name="google-site-verification" content="QB7w0bIGg_3VBosfpXwaiVc1c0hIxpQ0qRe6DC4Muj8" />
+            {/* Other meta tags */}
+            </Head>
+            <body>
+            <Main />
+            <NextScript />
+            </body>
+            </Html>
+    );
+    }
+}
+
+export default MyDocument;
