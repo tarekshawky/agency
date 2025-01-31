faa877c83c0a090b039e5c35c9aba4c96432775b
tarek <tarekshawky2016@gmail.com>
2025-02-01 00:00:11 +0200

    Add New Website

--- app/components/RecentWorks.tsx
+++ app/components/RecentWorks.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 'use client'
 import Image from 'next/image'
 import Link from "next/link";
@@ -8,10 +9,22 @@ export default function RecentWorks() {
                 <h2 className='font-bold text-3xl text-[#5f687b]'>Last Projects</h2>
             </div>
             <div className='container grid grid-cols-1 md:grid-cols-3 gap-8 mt-8'>
+                <Link href='https://iandsaccountingsvcs.com'  target='_blank' className='card-recent-work'>
+                    <Image className='w-full' src='/iands.png' alt="iandsaccountingsvcs" width={500} height={500}/>
+                    <div className='recent-work-description'>
+                        <h1>bookkeeping and taxes</h1>
+                        <p>
+                        I & S Accounting Services offers expert accounting solutions, including tax preparation, bookkeeping, and financial planning.
+                        </p>
+                        <button className='bg-primary text-white py-2 px-4 rounded-2xl'>
+                            Visit Website
+                        </button>
+                    </div>
+                </Link>
                 <Link href='https://albarzadxb.com/'  target='_blank' className='card-recent-work'>
                     <Image className='w-full' src='/img-1.jpeg' alt="albarzadxb.com" width={500} height={500}/>
                     <div className='recent-work-description'>
-                        <h2>Al Barza DXB</h2>
+                        <h1>Al Barza DXB</h1>
                         <p>
                             Al Barza DXB is a modern and dynamic website designed for a Dubai-based business, showcasing their services and offerings in an engaging manner.
                         </p>
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    "presets": ["next/babel"]
+
+};
 
 export default nextConfig;
